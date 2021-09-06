44a1f23f61eda20e0bddffdf76b5510aff62532b
admin <loikon911@gmail.com>
2021-09-06 10:35:34 +0700

    ok

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import ReCAPTCHA from "react-google-recaptcha";
+function App() {
+  function onChange(value) {
+    console.log("Captcha value:", value);
+  }
+  return (
+    <div className="App">
+      <ReCAPTCHA
+        sitekey="Your client site key"
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default App;
