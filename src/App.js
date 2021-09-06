bb6995f3cf8f1a583682c35072ddec4329a851d8
admin <loikon911@gmail.com>
2021-09-06 12:36:36 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import './App.css';
-import React, { useEffect } from 'react'
-
+import React from "react";
 import ReCAPTCHA from "react-google-recaptcha";
-function App() {
-  useEffect(()=>{
-    const script = document.createElement("script");
-    script.src =
-      "https://www.google.com/recaptcha/api.js";
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-  })
+import Captcha from "demos-react-captcha";
+import './App.css'
+const TEST_SITE_KEY = "6Lcg-EkcAAAAAIec-J_yxziH3okfLvqbYlkICNQT";
+const DELAY = 1500;
+
+function App () {
+ 
   function onChange(value) {
-    console.log("Captcha value:", value);
+    console.log('inputcaptcha:',value);
   }
-  return (
-    <div className="App">
-      <ReCAPTCHA
-        sitekey="6LdJ70kcAAAAADD0zHsSNDjM-WDyhj3VwxHblEdD"
-        onChange={onChange}
-      />
-    </div>
-  );
+    return (
+      <div className="App">
+          <ReCAPTCHA
+            style={{ display: "inline-block" }}
+            // ref={this._reCaptchaRef}
+            sitekey={TEST_SITE_KEY}
+            // onChange={this.handleChange}
+            // asyncScriptOnLoad={this.asyncScriptOnLoad}
+          />
+        <Captcha
+          style={{ display: "inline-block" }}
+          onChange={onChange}
+          placeholder="Enter captcha" // optional
+          length={6} // defaultn
+        />
+
+      </div>
+    );
+  
 }
+
+
+
 
 export default App;
