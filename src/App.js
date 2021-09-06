1f013b40bbea8ae38bd9adab79c234909f82a6a4
admin <loikon911@gmail.com>
2021-09-06 11:21:29 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
+import React, { useEffect } from 'react'
+
 import ReCAPTCHA from "react-google-recaptcha";
 function App() {
+  useEffect(()=>{
+    const script = document.createElement("script");
+    script.src =
+      "https://www.google.com/recaptcha/api.js";
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+  })
   function onChange(value) {
     console.log("Captcha value:", value);
   }
